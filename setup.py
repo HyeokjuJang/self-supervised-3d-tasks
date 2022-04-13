3b4f5ea8a4dcecfdf5cc87be5c80fd5943f97df0
HyeokjuJang <entaline@naver.com>
2022-04-13 16:49:41 +0900

    env setup updated

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
         'permutations': ['*.bin'],
     }, install_requires=['scikit-image', 'joblib', 'numpy==1.19.3', 'nibabel', 'scipy', 'pillow', 'pandas',
                          'matplotlib', 'seaborn', 'albumentations', 'tqdm', 'pydot', 'tensorflow-gpu==2.2.0', 'scikit-learn', 'hyperopt',
-                         'tensorflow_addons']
+                         'tensorflow_addons', 'h5py==2.10.0']
 )
